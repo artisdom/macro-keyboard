b671e7b1b52de78577ad0cd1a5cb3a3f3c9aac50
Alban Moreon <alban.moreon@protonmail.com>
2022-10-23 23:24:19 -0400

    Building keymap for first production unit

--- firmware/keyboards/home/keymap.c
+++ firmware/keyboards/home/keymap.c
@@ -9,11 +9,15 @@
 enum macro_keycodes {
     COPY,
     PASTE,
+    MOOM,
+    SNITCH,
 };
 
 uint16_t macros[][MACRO_LEN] = {
-    [COPY]  = { KC_LGUI, KC_C, KC_NO },
-    [PASTE] = { KC_LGUI, KC_V, KC_NO },
+    [COPY]   = { KC_LGUI, KC_C },
+    [PASTE]  = { KC_LGUI, KC_V },
+    [MOOM]   = { KC_LSFT, KC_LGUI, KC_4 },
+    [SNITCH] = { LCAG(KC_M)},
 };
 
 
@@ -21,7 +25,6 @@ uint16_t macros[][MACRO_LEN] = {
 enum layers {
     LAYER_0,
     LAYER_1,
-    LAYER_2,
     BT,
     BT_RST,
     LEDS,
@@ -34,19 +37,14 @@ enum layers {
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_0] = LAYOUT(
-        TO(BT),           KC_A,     KC_B,         KC_C,
-        TG(LAYER_1),    LSFT(KC_A), LSC(KC_DOWN), LAG(KC_DOWN),
-        xxxxxxx,          xxxxxxx,  RSC(KC_DOWN), xxxxxxx),
+        TO(BT),           xxxxxxx,       xxxxxxx,      LCTL(KC_SPC),
+        TG(LAYER_1),      MACRO(COPY),   MACRO(PASTE), M(SNITCH),
+        xxxxxxx,          LCTL(KC_LEFT), KC_CC_PLAY,   LCTL(KC_RGHT)),
 
     [LAYER_1] = LAYOUT(
-        TO(BT),           KC_AT,    KC_LPRN,  KC_RPRN,
-        TG(LAYER_2),      KC_D,     KC_E,     KC_F,
-        TG(LAYER_1),      M(COPY),  M(PASTE), KC_F),
-
-    [LAYER_2] = LAYOUT(
-        TO(BT),           KC_G,     KC_H,     KC_J,
-        xxxxxxx,          KC_G,     KC_H,     KC_J,
-        TG(LAYER_2),      _______,  _______,  _______),
+        TO(BT),           MACRO(MOOM),   KC_SPC, xxxxxxx,
+        xxxxxxx,          LALT(KC_LEFT), KC_4,   LALT(KC_RGHT),
+        TG(LAYER_1),      LGUI(KC_LEFT), KC_1,   LGUI(KC_RGHT)),
 
     [BT] = LAYOUT(
         TO(BT),           BT(0),    BT(1),     BT(2),
