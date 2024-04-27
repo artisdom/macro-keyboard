d3bdf84cccdf61a86f360e404097d248cd97d419
Alban Moreon <alban.moreon@protonmail.com>
2024-04-27 12:48:48 -0400

    Updated Home keyboard keymap

--- firmware/keyboards/home/keymap.c
+++ firmware/keyboards/home/keymap.c
@@ -14,6 +14,8 @@ enum macro_keycodes {
     SUBL_GROUP_R,
     SUBL_GROUP_L,
     SUBL_GROUP_FILTER,
+    SUBL_GROUP_MOVE_R,
+    SUBL_GROUP_MOVE_L,
 };
 
 
@@ -21,6 +23,8 @@ enum macro_keycodes {
 enum layers {
     LAYER_0,
     LAYER_SUBL,
+    LAYER_BLEND,
+    LAYER_3,
     LAYER_MOOM,
     BT,
     BT_RST,
@@ -35,6 +39,12 @@ enum layers {
 #define KC_MOOM LCA(KC_SPC)
 #define KC_FUSC LCG(KC_F)    // toggle fullscreen (macos)
 #define M_FUSC  KC_7         // toggle fullscreen (moom)
+#define SUBL_GOTO_DEF LAG(KC_DOWN)       // Sublime Text Goto Definition
+#define SUBL_GOTO_REF LSFT(LAG(KC_DOWN)) // Sublime Text Goto Definition
+#define SUBL_GOTO_BCK LCAG(KC_DOWN)      // Sublime Text Goto Backwards
+#define SUBL_SEL_EXPD SCMD(KC_A)         // Sublime Text Expand Selection
+#define SUBL_SEL_LINE LCMD(KC_L)         // Sublime Text Expand Selection to Line
+#define SUBL_SWAP_LINES LCG(KC_UP)       // Sublime Text Swap Line Up
 
 
 /* --------- Macros --------- */
@@ -46,6 +56,8 @@ const uint16_t macros[][MACRO_LEN] = {
     [SUBL_GROUP_R] = { LCMD(KC_K), LCMD(KC_RIGHT) },
     [SUBL_GROUP_L] = { LCMD(KC_K), LCMD(KC_LEFT) },
     [SUBL_GROUP_FILTER] = { LCMD(KC_K), LCMD(KC_F) },
+    [SUBL_GROUP_MOVE_R] = { LCMD(KC_K), S(KC_RIGHT) },   // Does not work... why?
+    [SUBL_GROUP_MOVE_L] = { KC_LCMD, KC_K, S(KC_LEFT) }, // Does not work... why?
 };
 
 
@@ -53,14 +65,26 @@ const uint16_t macros[][MACRO_LEN] = {
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_0] = LAYOUT(
         TO(BT),           MACRO(MOOML),  xxxxxxx,      LCTL(KC_SPC),
-        TG(LAYER_SUBL),   MACRO(COPY),   MACRO(PASTE), M(SNITCH),
-        xxxxxxx,          LCTL(KC_LEFT), KC_PLPS,      LCTL(KC_RGHT)
+        TG(LAYER_SUBL),   xxxxxxx,       KC_PLPS,      M(SNITCH),
+        xxxxxxx,          LCTL(KC_LEFT), LCTL(KC_UP),  LCTL(KC_RGHT)
     ),
 
     [LAYER_SUBL] = LAYOUT(
-        TO(BT),           xxxxxxx,         LSFT(LAG(KC_DOWN)), M(SUBL_GROUP_FILTER),
-        TG(LAYER_MOOM),   M(SUBL_GROUP_L),   LCAG(KC_DOWN),    M(SUBL_GROUP_R),
-        TG(LAYER_SUBL),   LAG(KC_LEFT),       LAG(KC_DOWN),    LAG(KC_RIGHT)
+        TO(BT),           SUBL_SEL_EXPD,    SUBL_GOTO_REF,  M(SUBL_GROUP_FILTER),
+        TG(LAYER_BLEND),  SUBL_SEL_LINE,    SUBL_GOTO_BCK,  SUBL_SWAP_LINES,
+        TG(LAYER_SUBL),   M(SUBL_GROUP_L),  SUBL_GOTO_DEF,  M(SUBL_GROUP_R)
+    ),
+
+    [LAYER_BLEND] = LAYOUT(
+        TO(BT),           KC_X,      KC_Y,      KC_Z,
+        TG(LAYER_3),      xxxxxxx,   KC_LSFT,   LCMD(KC_P0),
+        TG(LAYER_BLEND),  KC_PSLS,   KC_KP_DOT, KC_P0
+    ),
+
+    [LAYER_3] = LAYOUT(
+        TO(BT),           xxxxxxx,   xxxxxxx,   xxxxxxx,
+        TG(LAYER_MOOM),   xxxxxxx,   xxxxxxx,   xxxxxxx,
+        TG(LAYER_3),      xxxxxxx,   xxxxxxx,   xxxxxxx
     ),
 
     [LAYER_MOOM] = LAYOUT(
